b59fbd0ef9e3a48ece9640bed075b916819a0bdc
ashutoshver <ashutosheng1412@gmail.com>
2022-12-05 11:59:50 +0530

    comm

--- script.js
+++ script.js
@@ -74,7 +74,6 @@ const displayUser = () => {
     <h3><span>Pan Number:</span>${pan}</h3>
     <p><span>Age:</span>${age}</p>
     <p><span>Highest Qualification:</span>${qualification}</p>`;
-
   })
 }
 
